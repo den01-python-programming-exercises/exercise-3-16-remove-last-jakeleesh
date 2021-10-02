436474c21378d23ed45ef4ef777cee79175ebecf
Jake Lee <jake.lee@fdmgroup.com>
2021-10-02 22:53:37 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
+    strings = []
+
+    strings.append("First")
+    strings.append("Second")
+    strings.append("Third")
+
+    print(strings)
+
+    remove_last(strings)
+    remove_last(strings)
+
+    print(strings)
+
+def remove_last(strings):
+    if (len(strings) == 0):
+        return
+    else:
+        strings.pop(-1)
 
 if __name__ == '__main__':
     main()
